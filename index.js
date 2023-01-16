acd8bba91a436e0b40f7987e4a28ac89d67c8d11
HexCrunch <wahib.mkadmi@esprit.tn>
2023-01-16 03:08:12 +0100

    one last push

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-const path = require('path');
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const require = createRequire(import.meta.url);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const mstsc = require(path.join(__dirname, 'index.node'));
 export default mstsc;
